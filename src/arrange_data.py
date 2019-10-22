2fc07dcb10877832f979228b294b757974028e28
Daniel Gonzalez <daniel.gonzalez.alv@gmail.com>
2019-10-22 14:41:38 +0200

    load MNIST data-set

--- src/arrange_data.py
+++ src/arrange_data.py
@@ -7,7 +7,8 @@ Created on Fr July 12 18:03:28 2018
 @author: TDA-Team:  Sebastian Dobrzynski, Daniel Gonzalez, Andre Schulze
 
 This script loads the data, shuffle it, and generate traing/test 
-data set 
+data set.
+Because we want to have the hole data in the same file, we merge everything together at the end.
 """
 
 # Load dependencies
@@ -49,19 +50,23 @@ def prep_MNIST():
     X_train, X_test, y_train , y_test = \
         model_selection.train_test_split(
                 digits.data, digits.target,
-                test_size = 0.1, 
+                test_size = 0.4, 
                 random_state = 0)
     return X_train, X_test, y_train, y_test
 
 if __name__ == "__main__":
     X_train, X_test, y_train , y_test= prep_MNIST()
+    X_all = np.append(X_train, X_test, axis= 0)
+    y_all = np.append(y_train, y_test, axis=0)
     X_300 = np.append(X_train[0:240], X_test[0:60], axis=0)
     y_300 = np.append(y_train[0:240], y_test[0:60], axis=0)
     
     # Save npy files
-   # np.save("../data/data_set/X_train", X_train)
-   # np.save("../data/data_set/y_train", y_train)
-   # np.save("../data/data_set/X_test", X_test)
-   # np.save("../data/data_set/y_test", y_test)
-   # np.save("../data/data_set/X_300", X_300)
-   # np.save("../data/data_set/y_300", y_300)
+    np.save("../data/data_set/X_train", X_train)
+    np.save("../data/data_set/y_train", y_train)
+    np.save("../data/data_set/X_test", X_test)
+    np.save("../data/data_set/y_test", y_test)
+    np.save("../data/data_set/X_300", X_300)
+    np.save("../data/data_set/y_300", y_300)
+    np.save("../data/data_set/X_all", X_all)
+    np.save("../data/data_set/y_all", y_all)
